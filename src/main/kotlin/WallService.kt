7c074a1dca39835b5a3ec9db937c4e55fe4b45ea
sergey-shiryaev <s.shiryaev96@mail.ru>
2022-04-03 17:19:18 +0300

    Rework added

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -12,32 +12,22 @@ object WallService {
     }
 
     fun update(updatePost: Post): Boolean {
-        for (post in posts) {
+        for ((index, post) in posts.withIndex()) {
             if (post.id == updatePost.id) {
                 val newUpdatePost: Post = post.copy(
                     owner_id = updatePost.owner_id,
-                    from_id = updatePost.from_id,
-                    created_by = updatePost.created_by,
                     text = updatePost.text,
-                    reply_owner_id = updatePost.reply_owner_id,
-                    reply_post_id = updatePost.reply_post_id,
-                    friends_only = updatePost.friends_only,
                     comments = updatePost.comments,
                     likes = updatePost.likes,
                     reposts = updatePost.reposts,
                     views = updatePost.views,
                     post_type = updatePost.post_type,
                     signer_id = updatePost.signer_id,
-                    can_pin = updatePost.can_pin,
-                    can_delete = updatePost.can_delete,
-                    can_edit = updatePost.can_edit,
                     is_pinned = updatePost.is_pinned,
                     marked_as_ads = updatePost.marked_as_ads,
-                    is_favorite = updatePost.is_favorite,
-                    postponed_id = updatePost.postponed_id
+                    is_favorite = updatePost.is_favorite
                 )
-                posts.remove(post)
-                posts += newUpdatePost
+                posts[index] = newUpdatePost
                 return true
             }
         }
